73b22989432790cc2a752eacdc59c38199285653
uXMs Devs <info@uxms.net>
2016-02-10 13:16:27 +0200

    Indentations changed for PSR Support

--- Plugin.php
+++ Plugin.php
@@ -9,98 +9,98 @@ use Uxms\Sharecount\Components\ShareCount;
 
 class Plugin extends PluginBase
 {
-	public function pluginDetails()
-	{
-		return [
-			'name'        => 'uxms.sharecount::lang.app.name',
-			'description' => 'uxms.sharecount::lang.app.desc',
-			'author'      => 'uXMs Devs',
-			'icon'        => 'icon-group',
-			'homepage'    => 'https://uxms.net/'
-		];
-	}
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'uxms.sharecount::lang.app.name',
+            'description' => 'uxms.sharecount::lang.app.desc',
+            'author'      => 'uXMs Devs',
+            'icon'        => 'icon-group',
+            'homepage'    => 'https://uxms.net/'
+        ];
+    }
 
-	public function registerPermissions()
-	{
-		return [
-			'uxms.sharecount.address' => [
-				'label' => 'Access Webpages',
-				'tab' => 'Share Count'
-			]
-		];
-	}
+    public function registerPermissions()
+    {
+        return [
+            'uxms.sharecount.address' => [
+                'label' => 'Access Webpages',
+                'tab' => 'Share Count'
+            ]
+        ];
+    }
 
-	public function registerComponents()
-	{
-		return [
-			'Uxms\Sharecount\Components\ShareCount' => 'shareCount'
-		];
-	}
+    public function registerComponents()
+    {
+        return [
+            'Uxms\Sharecount\Components\ShareCount' => 'shareCount'
+        ];
+    }
 
-	public function registerSettings()
-	{
-		return [
-			'config' => [
-				'label'       => 'uxms.sharecount::lang.app.name',
-				'description' => 'uxms.sharecount::lang.app.setting_desc',
-				'icon'        => 'icon-group',
-				'class'       => 'Uxms\Sharecount\Models\Configs',
-				'category'    => SettingsManager::CATEGORY_SOCIAL,
-				'order'       => 998
-			]
-		];
-	}
+    public function registerSettings()
+    {
+        return [
+            'config' => [
+                'label'       => 'uxms.sharecount::lang.app.name',
+                'description' => 'uxms.sharecount::lang.app.setting_desc',
+                'icon'        => 'icon-group',
+                'class'       => 'Uxms\Sharecount\Models\Configs',
+                'category'    => SettingsManager::CATEGORY_SOCIAL,
+                'order'       => 998
+            ]
+        ];
+    }
 
-	/**
-	 * Not yet working in v1.0.0
-	 */
-	public function registerSchedule($schedule)
-	{
-		$schedule->call(function() {
-			ShareCount::instance()->updateShareCounts();
-		})->everyFiveMinutes();
-	}
+    /**
+     * Not yet working in v1.0.0
+     */
+    public function registerSchedule($schedule)
+    {
+        $schedule->call(function() {
+            ShareCount::instance()->updateShareCounts();
+        })->everyFiveMinutes();
+    }
 
-	public function registerNavigation()
-	{
-		return [
-			'sharecount' => [
-				'label'       => 'uxms.sharecount::lang.app.menu_label',
-				'url'         => Backend::url('uxms/sharecount/address'),
-				'icon'        => 'icon-group',
-				'permissions' => ['uxms.sharecount.*'],
-				'order'       => 998,
+    public function registerNavigation()
+    {
+        return [
+            'sharecount' => [
+                'label'       => 'uxms.sharecount::lang.app.menu_label',
+                'url'         => Backend::url('uxms/sharecount/address'),
+                'icon'        => 'icon-group',
+                'permissions' => ['uxms.sharecount.*'],
+                'order'       => 998,
 
-				'sideMenu' => [
-					'address' => [
-						'label'       => 'uxms.sharecount::lang.addresses.page_title',
-						'icon'        => 'icon-connectdevelop',
-						'url'         => Backend::url('uxms/sharecount/address'),
-						'permissions' => ['uxms.sharecount.address']
-					]
-				]
-			]
-		];
-	}
+                'sideMenu' => [
+                    'address' => [
+                        'label'       => 'uxms.sharecount::lang.addresses.page_title',
+                        'icon'        => 'icon-connectdevelop',
+                        'url'         => Backend::url('uxms/sharecount/address'),
+                        'permissions' => ['uxms.sharecount.address']
+                    ]
+                ]
+            ]
+        ];
+    }
 
-	/**
-	 * The boot() method is called right before a request is routed
-	 */
-	public function boot()
-	{
-		if ( Configs::get('cache_time_out') == null ) {
-			Configs::set('cache_time_out', 'daily');
-		}
+    /**
+     * The boot() method is called right before a request is routed
+     */
+    public function boot()
+    {
+        if ( Configs::get('cache_time_out') == null ) {
+            Configs::set('cache_time_out', 'daily');
+        }
 
-		if ( Configs::get('fetch_with_cron') == null ) {
-			Configs::set('fetch_with_cron', '1');
-		}
+        if ( Configs::get('fetch_with_cron') == null ) {
+            Configs::set('fetch_with_cron', '1');
+        }
 
-		if ( Configs::get('timezone') == null ) {
-			Configs::set('timezone', 'America/Los_Angeles');
-		}
+        if ( Configs::get('timezone') == null ) {
+            Configs::set('timezone', 'America/Los_Angeles');
+        }
 
-		date_default_timezone_set(Configs::get('timezone'));
-	}
+        date_default_timezone_set(Configs::get('timezone'));
+    }
 
 }
--- updates/create_sharecount_webpages.php
+++ updates/create_sharecount_webpages.php
@@ -5,23 +5,23 @@ use October\Rain\Database\Updates\Migration;
 
 class CreateSharecountMappings extends Migration
 {
-	public function up()
-	{
-		Schema::create('uxms_sharecount_webpages', function ($table) {
-			$table->engine = 'InnoDB';
-			$table->increments('id')->index();
-			$table->text('url');
-			$table->integer('count_face')->default(0);
-			$table->integer('count_twit')->default(0);
-			$table->integer('count_gp')->default(0);
-			$table->timestamp('last_fetched')->nullable();
-			$table->timestamps();
-		});
-	}
+    public function up()
+    {
+        Schema::create('uxms_sharecount_webpages', function ($table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->index();
+            $table->text('url');
+            $table->integer('count_face')->default(0);
+            $table->integer('count_twit')->default(0);
+            $table->integer('count_gp')->default(0);
+            $table->timestamp('last_fetched')->nullable();
+            $table->timestamps();
+        });
+    }
 
-	public function down()
-	{
-		Schema::dropIfExists('uxms_sharecount_webpages');
-	}
+    public function down()
+    {
+        Schema::dropIfExists('uxms_sharecount_webpages');
+    }
 
 }
